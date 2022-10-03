2eb92195bfe01df0f76863b9c1a4a64f917a40a8
Omkar Gavhane <ogomkargavhane@gmail.com>
2022-10-03 09:08:05 +0530

    commit

--- src/components/ChatContainer/ChatContainer.jsx
+++ src/components/ChatContainer/ChatContainer.jsx
@@ -35,7 +35,7 @@ const ChatContainer = ({
   const API =
     window.location.host === "localhost:3000"
       ? "http://localhost:8080"
-      : "https://whatsup-api-77.herokuapp.com/";
+      : "https://whatsup-api-77.herokuapp.com";
   const SOCKET_API =
     window.location.host === "localhost:3000"
       ? "ws://localhost:8900"
